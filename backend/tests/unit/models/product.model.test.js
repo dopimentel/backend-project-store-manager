dcd6d33a79c66561084afd6dbf07180b577638aa
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-01 19:20:47 -0300

    feat: lint

--- backend/tests/unit/models/product.model.test.js
+++ backend/tests/unit/models/product.model.test.js
@@ -4,20 +4,16 @@ const connection = require('../../../src/models/connection');
 
 const { productFromDB, productsFromDB } = require('../mocks/product.mock');
 
-
 describe('Realizando testes - PRODUCT MODEL:', function () {
   it('Verificando se a função getAll retorna um array', async function () {
     sinon.stub(connection, 'execute').resolves([productsFromDB]);
     const products = await connection.execute();
     expect(products).to.be.an('array');
-  }
-  );
+  });
 
   it('Verificando se a função findById retorna um objeto', async function () {
     sinon.stub(connection, 'execute').resolves([productFromDB]);
     const product = await connection.execute();
     expect(product).to.be.an('object');
-  }
-  );
-
+  });
 });
