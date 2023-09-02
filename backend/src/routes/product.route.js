0098cc1e92f68ace4ac6a6223fc4f22339c75781
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-02 12:43:43 -0300

    feat: Requisito 4 - parcial

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -1,8 +1,9 @@
 const route = require('express').Router();
 const { productController } = require('../controllers');
+const { validateProduct } = require('../middlewares/validationsInputValues');
 
 route.get('/', productController.getAll);
 route.get('/:id', productController.findById);
-route.post('/', productController.create);
+route.post('/', validateProduct, productController.create);
 
 module.exports = route;
