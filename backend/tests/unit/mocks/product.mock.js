cb57ebbba8f839b6a64d6227df10dba9a851de8f
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-01 23:37:47 -0300

    feat: Requisito 1 - testes

--- backend/tests/unit/mocks/product.mock.js
+++ backend/tests/unit/mocks/product.mock.js
@@ -24,8 +24,34 @@ const productFromDB = {
 
 const productFromDBIdNotExists = [];
 
+const productsFromModel = [
+  {
+    id: 1,
+    name: 'Skol Lata 250ml',
+  },
+  {
+    id: 2,
+    name: 'Heineken 600ml',
+  },
+  {
+    id: 3,
+    name: 'Antarctica Pilsen 300ml',
+  },
+  {
+    id: 4,
+    name: 'Brahma 600ml',
+  },
+];
+
+const productFromModel = {
+  id: 1,
+  name: 'Skol Lata 250ml',
+};
+
 module.exports = {
   productFromDB,
   productsFromDB,
   productFromDBIdNotExists,
+  productsFromModel,
+  productFromModel,
 };
--- backend/tests/unit/services/product.service.test.js
+++ backend/tests/unit/services/product.service.test.js
@@ -1,7 +1,20 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
+const { productModel } = require('../../../src/models');
+const { productsFromModel } = require('../mocks/product.mock');
+const { productService } = require('../../../src/services');
 
 
 describe('Realizando testes - PRODUCT SERVICE:', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+  it('Verificando se a função getAll retorna um todos os produtos', async function () {
+    sinon.stub(productModel, 'getAll').resolves(productsFromModel);
+    
+    const serviceResponse = await productService.getAll();
+    
+    expect(serviceResponse).to.be.an('object');
+  });
 
 });
