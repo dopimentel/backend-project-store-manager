c03755defac94e455ebc14f18a4283efe2e0c322
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-01 23:54:41 -0300

    feat: testes

--- backend/tests/unit/services/product.service.test.js
+++ backend/tests/unit/services/product.service.test.js
@@ -15,4 +15,13 @@ describe('Realizando testes - PRODUCT SERVICE:', function () {
     
     expect(serviceResponse).to.be.an('object');
   });
+  it('Verificando se a função findById retorna um produto quando o ID existe', async function () {
+    sinon.stub(productModel, 'findById').resolves(productsFromModel[0]);
+
+    const serviceResponse = await productService.findById(1);
+
+    expect(serviceResponse).to.be.an('object');
+    expect(serviceResponse.status).to.be.equal('SUCCESSFUL');
+    expect(serviceResponse.data).to.deep.equal(productsFromModel[0]);
+  });
 });
