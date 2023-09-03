6f8e40c6a71a289bfd9b210e2ec6a63c5c4edae3
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-03 18:05:24 -0300

    feat: Requisito 7

--- backend/tests/unit/models/product.model.test.js
+++ backend/tests/unit/models/product.model.test.js
@@ -9,14 +9,15 @@ describe('Realizando testes - PRODUCT MODEL:', function () {
   afterEach(function () {
     sinon.restore();
   });
-  it('Verificando se a função getAll retorna um array', async function () {
+  it('Listando os produtos com sucesso e retornar um array', async function () {
     sinon.stub(connection, 'execute').resolves([productsFromDB]);
     const products = await connection.execute();
-    expect(products).to.be.an('array');
-    expect(products).to.deep.equal([productsFromDB]);
+    expect(products[0]).to.be.an('array');
+    expect(products[0]).to.deep.equal(productsFromDB);
+    expect(products[0]).to.have.lengthOf(4);
   });
 
-  it('Verificando se a função findById retorna um objeto quando o ID existe', async function () {
+  it('Recuperando um produto pelo ID com sucesso e retornar um objeto', async function () {
     sinon.stub(connection, 'execute').resolves([[productFromDB]]);
     const product = await productModel.findById(1);
     expect(product).to.be.an('object');
@@ -26,6 +27,34 @@ describe('Realizando testes - PRODUCT MODEL:', function () {
   it('Verificando se a função findById retorna null quando o ID não existe', async function () {
     sinon.stub(connection, 'execute').resolves([productFromDBIdNotExists]);
     const product = await productModel.findById(10000);
+    expect(product).to.equal(null);
+  });
+  it('Insere um produto com sucesso e retorna com o id inserido', async function () {
+    sinon.stub(connection, 'execute').resolves([{ insertId: 1 }]);
+    const productId = await productModel.create('Produto Teste');
+    expect(productId).to.be.an('number');
+    expect(productId).to.equal(1);
+  });
+  it('Atualiza um produto com sucesso e retorna um objeto', async function () {
+    sinon.stub(connection, 'execute').resolves([[productFromDB]]);
+    const product = await productModel.update(1, 'Produto Teste');
+    expect(product).to.be.an('object');
+    expect(product).to.have.all.keys('id', 'name');
+  });
+  it('Verificando se a função update retorna null quando o ID não existe', async function () {
+    sinon.stub(connection, 'execute').resolves([productFromDBIdNotExists]);
+    const product = await productModel.update(10000, 'Produto Teste');
+    expect(product).to.equal(null);
+  });
+  it('Deleta um produto com sucesso e retorna um objeto', async function () {
+    sinon.stub(connection, 'execute').resolves([[productFromDB]]);
+    const product = await productModel.deleteProduct(1);
+    expect(product).to.be.an('object');
+    expect(product).to.have.all.keys('id', 'name');
+  });
+  it('Verificando se a função deleteProduct retorna null quando o ID não existe', async function () {
+    sinon.stub(connection, 'execute').resolves([productFromDBIdNotExists]);
+    const product = await productModel.deleteProduct(10000);
     expect(product).to.equal(null);
   });
 });
